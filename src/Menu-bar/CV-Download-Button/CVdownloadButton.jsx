2303f206fbfb017844e592d0b021583d225f6d21
Chris Blanco <chrisblanco.2004@gmail.com>
2026-02-25 12:34:54 -0600

    Menu bar design finished

--- src/Menu-bar/CV-Download-Button/CVdownloadButton.jsx
+++ src/Menu-bar/CV-Download-Button/CVdownloadButton.jsx
@@ -2,7 +2,7 @@ import './CVdownloadButton.css'
 
 function CVdownloadButton() {
     return (
-        <div id='downloadCVbutton-container' >
+        <div id='downloadCVbutton-container'>
             <img src='./src/assets/download.png'></img>
             <button id='downloadCVbutton'>Download CV</button>
         </div>
