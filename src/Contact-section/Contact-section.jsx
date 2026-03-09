188147fbf05e893698d8840ef88af101a3af8371
Chris Blanco <chrisblanco.2004@gmail.com>
2026-03-09 11:47:09 -0600

    minor footer changes

--- src/Contact-section/Contact-section.jsx
+++ src/Contact-section/Contact-section.jsx
@@ -1,28 +1,33 @@
 import './Contact-section.css'
 
+import linkedinIcon from '../assets/linkedin.png'
+import githubFooterIcon from '../assets/github-footer.png'
+import emailIcom from '../assets/gmail.png'
+import whatsAppIcon from '../assets/whatsapp.png'
+
 function ContactSection() {
     return (
         <div id='contact-section-container'>
             <h3>Feel free to contact me!</h3>
             <ul id='contact-list'>
                 <li className='contact-list-item-rl'>
-                    <a href='https://www.linkedin.com/in/christopher-blanco-b7338a272/'>
-                        <img src='./src/assets/linkedin.png'></img>
+                    <a href='https://www.linkedin.com/in/christopher-blanco-b7338a272/' target='_blank' rel='noreferrer'>
+                        <img src={linkedinIcon}></img>
                     </a>
                 </li>
                 <li className='contact-list-item-rr'>
-                    <a href='https://github.com/christopherblanco'>
-                        <img src='./src/assets/github-footer.png'></img>
+                    <a href='https://github.com/christopherblanco' target='_blank' rel='noreferrer'>
+                        <img src={githubFooterIcon}></img>
                     </a>
                 </li>
                 <li className='contact-list-item-rl'>
-                    <a href='mailto:christopherblanco@gmail.com'>
-                        <img src='./src/assets/gmail.png'></img>
+                    <a href='mailto:christopherblanco@gmail.com' target='_blank' rel='noreferrer'>
+                        <img src={emailIcom}></img>
                     </a>
                 </li>
                 <li className='contact-list-item-rr'>
-                    <a href='mailto:christopherblanco@gmail.com'>
-                        <img src='./src/assets/whatsapp.png'></img>
+                    <a href='https://wa.me/50671840668' target='_blank' rel='noreferrer'>
+                        <img src={whatsAppIcon}></img>
                     </a>
                 </li>
             </ul>
